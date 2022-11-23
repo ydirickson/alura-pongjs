831214f0852fc0b9d119efc3ba936687e4f28790
Yuri Dirickson <yuri.dirickson@usp.br>
2022-11-23 14:10:21 -0300

    Adicionando tamanho do raio na comparação de colisão

--- jogo.js
+++ jogo.js
@@ -1,11 +1,13 @@
+let height = 400
+let width = 600
 
 let xBolinha = 300
 let yBolinha = 200
 let diametro = 15
+let raio = diametro / 2
+
 let velocidadeXBolinha = 6
 let velocidadeYBolinha = 6
-let height = 400
-let width = 600
 
 const setup = () => {
   createCanvas(width, height)
@@ -17,10 +19,10 @@ const draw = () => {
   xBolinha += velocidadeXBolinha
   yBolinha += velocidadeYBolinha
 
-  if (xBolinha > width || xBolinha < 0) {
+  if (xBolinha+raio > width || xBolinha-raio < 0) {
     velocidadeXBolinha *= -1;
   }
-  if (yBolinha > height || yBolinha < 0) {
+  if (yBolinha+raio > height || yBolinha-raio < 0) {
       velocidadeYBolinha *= -1;
   }
 }
